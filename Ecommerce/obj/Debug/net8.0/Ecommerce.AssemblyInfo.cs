4586306c56474a316eb73be3955f64ae944293a4
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-25 20:19:23 -0300

    Criando as models usuário e produto com os códigos de classes

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6154e3d714ab090f18769709693bb1540b899eb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+960c9ec99f5741e3aa24cba1765b4e64463c6015")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
