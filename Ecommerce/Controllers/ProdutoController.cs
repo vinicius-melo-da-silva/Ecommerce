4ad4454d599219cd8d9fbbd716cbf1e002f1f583
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-25 20:40:39 -0300

    Criando as controllers usuário e produto com as interfaces, validar com o professor o produto controller

---
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+// TERMINAR A CONTROLLER PRODUTOS
---
+++ Ecommerce/Controllers/UsuarioController.cs
@@ -0,0 +1,65 @@
+﻿using Ecommerce.Models;
+using Ecommerce.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class UsuarioController : Controller
+    {
+
+        //Declara uma variavel privada somento leitura
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        //Construtor
+        public LoginController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost] //Manda os dados, envia o usuário e senha 
+        public IActionResult Login(String email, string senha)
+        {
+            //Ele usa _usuarioRepositorio.ObterUsuario(email) para buscar o usuário no banco de dados.
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            //Se o usuário for encontrado e a senha estiver correta, ele redireciona o usuário para a página inicial (RedirectToAction("Index", "Home")).
+            if (usuario != null && usuario.Senha == senha) //Esse simbolo != é sinal de diferente
+            {
+                // Autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            //Caso contrário, ele adiciona um erro ao ModelState e retorna a visualização de login, exibindo a mensagem de erro.
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+
+            //retorna a visualização (a página HTML) associada a este método
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            //verifica se os dados do usuário são válidos (de acordo com as regras de validação definidas na classe Usuario).
+            if (ModelState.IsValid)
+            {
+                // Se os dados forem válidos, ele usa _usuarioRepositorio.
+                // AdicionarUsuario(usuario) para adicionar o novo usuário ao banco de dados e redireciona para a página de login.
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+
+                // visualização de cadastro, exibindo os erros de validação
+
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
+        }
+    }
+}
