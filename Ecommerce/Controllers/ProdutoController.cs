b6a0dd447a04cc74a6ddf53410a8bb9e088cd4a5
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-26 20:04:44 -0300

    Adicionando viws de cadastro de produto, validar com o pŕofessor a controller produto

--- Ecommerce/Controllers/ProdutoController.cs
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -1,14 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecommerce.Models;
+using Ecommerce.Repositorio;
+using Ecommerce.Repositorio.Ecommerce.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecommerce.Controllers
 {
+
     public class ProdutoController : Controller
     {
-        public IActionResult Index()
+        //Declara uma variavel privada somento leitura
+        private readonly ProdutoRepositorio _produtoRepositorio;
+      
+        //Construtor
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+
+        public IActionResult Cadastro_Produto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro_Produto()
         {
             return View();
         }
     }
 }
-
-// TERMINAR A CONTROLLER PRODUTOS
--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4454d599219cd8d9fbbd716cbf1e002f1f583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfec274271c4204e1a8b9110c85fd7849057531")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
