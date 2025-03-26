bcfec274271c4204e1a8b9110c85fd7849057531
Vinicius <viniciusmsilva.ds@gmail.com>
2025-03-26 19:09:49 -0300

    Adicionando as viws de login e cadastro do usuário

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159ea3e0d22177826001ba20102ddfd66e592308")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad4454d599219cd8d9fbbd716cbf1e002f1f583")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
